21de45d72e1c2ca8a44a759332a3675e73b06cd0
Shikhar Srivastava <shikharsr100@gmail.com>
2026-08-08 23:56:29 +0530

    Add select element to app

--- app.py
+++ app.py
@@ -8,28 +8,35 @@ st.set_page_config(
     layout="centered",
 )
 
-selected_model = "logistic_regression"
-stats = evaluate_model(selected_model)
+model_map = {
+    "Logistic Regression": "logistic_regression",
+    "Decision Tree": "decision_tree",
+    "K Nearest Neighbors": "knn",
+    "Naive Bayes": "naive_bayes",
+    "Random Forest": "random_forest",
+}
 
+st.title("ML Classification Models Showcase")
+st.space("small")
+
+selected_model = st.selectbox("Select Classifier Model", model_map.keys())
+st.space("small")
+
+st.header(f"Calculated Metrics for {selected_model}")
+
+stats = evaluate_model(model_map[selected_model])
 stats = [i for i in stats.items()]
-stats = stats[1::]
 
-with st.container(horizontal=False, gap="medium"):
-    for i in range(0, 6, 2):
-        metric1, metric2 = stats[i], stats[i+1]
+if (len(stats)):
+    stats = stats[1::]
 
-        cols = st.columns(2, gap="medium", width=400)
+    with st.container(horizontal=False, gap="medium"):
+        for i in range(0, 6, 2):
+            metric1, metric2 = stats[i], stats[i+1]
 
-        with cols[0]:
-            st.metric(
-                metric1[0],
-                metric1[1],
-                width="content",
-            )
+            cols = st.columns(2, width="stretch", gap="medium")
 
-        with cols[1]:
-            st.metric(
-                metric2[0],
-                metric2[1],
-                width="content",
-            )
+            with cols[0]:
+                st.metric(metric1[0], f"{metric1[1]:.4f}", width="content")
+            with cols[1]:
+                st.metric(metric2[0], f"{metric2[1]:.4f}", width="content")
